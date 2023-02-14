d8a484f10dc04359d94792089d09990f5ced6a66
Hanzhi Zhou <hanzhizhou@bytedance.com>
2023-02-14 07:11:16 +0000

    Merge branch 'misc' into 'master'
    misc changes
    
    See merge request data/monolith!1966
    
    GitOrigin-RevId: fac09dcc2f25d67645b5ca4e756ba2a33b726ed1

--- monolith/native_training/model_comp_test.py
+++ monolith/native_training/model_comp_test.py
@@ -13,18 +13,17 @@
 # limitations under the License.
 
 import os
-# MONOLITH_WITH_HOROVOD has to be set before monolith.native_training.
+local_size = int(os.environ.get('OMPI_COMM_WORLD_LOCAL_SIZE', 1))
+rank = int(os.environ.get('OMPI_COMM_WORLD_RANK', 0))
+os.environ['CUDA_VISIBLE_DEVICES'] = str(rank % local_size)
 os.environ["MONOLITH_WITH_HOROVOD"] = "True"
-#os.environ["MONOLITH_WITH_OPTIMIZED_HOROVOD"] = "True"
 os.environ["HOROVOD_AUTOTUNE"] = "1"
 os.environ["HOROVOD_CYCLE_TIME"] = "0.1"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 os.environ["MONOLITH_SYNC_EMPTY_RANK0_PS_SHARD"] = "0"
 os.environ["MONOLITH_WITH_ALLREDUCE_FUSION"] = "one"
 os.environ['MONOLITH_ROOT_LOG_INTERVAL'] = "10"
 
 import time
-import numpy as np
 import tensorflow as tf
 tf.compat.v1.set_random_seed(42)
 
@@ -37,12 +36,7 @@ from monolith.native_training.data.training_instance.python.parser_utils import 
 from monolith.native_training.entry import (AdagradOptimizer, Fp16Compressor,
                                             ZerosInitializer)
 from monolith.native_training import layers
-
-
 import horovod.tensorflow as hvd
-import random
-import numpy as np
-
 
 deep = {
     "initializer":
@@ -56,15 +50,15 @@ deep = {
         Fp16Compressor()
 }
 
-num_features = 10
-batch_size = 345
-emb_dim = 16
+num_features = 17
+batch_size = 455
+emb_dim = 15
 feature_names = [f'feature{i}' for i in range(num_features)]
-fid_max_val = 1024000
+fid_max_val = 100000
 
 
 def lookup_tf_embedding(features, f_name, dim):
-  f = tf.RaggedTensor.from_row_splits(features[f'tf_{f_name}_p1'], features[f'tf_{f_name}_p2'])
+  f = tf.RaggedTensor.from_row_splits(features[f'tf_{f_name}_p1'], features[f'tf_{f_name}_p2'], validate=False)
   embeddings = tf.nn.embedding_lookup(
       params=tf.Variable(initial_value=tf.zeros(shape=(fid_max_val + 1, dim))), 
       ids=f.values)
@@ -75,50 +69,36 @@ class EmbeddingUpdateTask(MonolithModel):
   
   def __init__(self, params=None):
     super(EmbeddingUpdateTask, self).__init__(params)
-    self.train.max_steps = 20
+    self.train.max_steps = 50
     self.train.per_replica_batch_size = batch_size
 
   def input_fn(self, mode):
 
-    def input_gen():
-      while True:
-        features = {}
-        for i in range(num_features):
-          features[f'feature{i}'] = np.random.randint(0, fid_max_val, size=random.randint(1, 25), dtype=np.int64)
-        features['label'] = random.choice([0.0, 1.0])
-        yield features
-
     def decomp_func(features):
-      # note: this is a workaround to pass fids to model_fn, since all instances if RaggedTensor are gone
+      # note: this is a workaround to pass fids to model_fn, since all instances of RaggedTensor are gone
       for i in range(num_features):
         features[f'tf_feature{i}_p1'] = features[f'feature{i}'].values
         features[f'tf_feature{i}_p2'] = features[f'feature{i}'].row_splits
+      return advanced_parse(features)
+
+    @tf.function
+    def input_tensors():
+      features = {}
+      for i in range(num_features):
+        features[f'feature{i}'] = tf.random.uniform((tf.random.uniform((), 1, 25, dtype=tf.int32),), 0, fid_max_val, dtype=tf.int64)
+      features['label'] = tf.cast(tf.random.uniform((), 0, 2, dtype=tf.int32), tf.float32)
       return features
+
+    return tf.data.experimental.Counter(0, 1).map(lambda _: input_tensors(), tf.data.AUTOTUNE).\
+      apply(tf.data.experimental.dense_to_ragged_batch(batch_size, True)).\
+        map(decomp_func, tf.data.AUTOTUNE).prefetch(tf.data.AUTOTUNE)
     
-    output_signatures = {'label': tf.TensorSpec(())}
-    for i in range(num_features):
-      output_signatures[f'feature{i}'] = tf.TensorSpec((None,), dtype=tf.int64)
-      
-    return tf.data.Dataset.from_generator(input_gen, output_signature=output_signatures)\
-      .apply(tf.data.experimental.dense_to_ragged_batch(batch_size, True))\
-        .map(decomp_func).map(advanced_parse, tf.data.AUTOTUNE).prefetch(tf.data.AUTOTUNE)
-        
-    # def input_tensors():
-    #   features = {}
-    #   for i in range(num_features):
-    #     features[f'feature{i}'] = tf.random.uniform((tf.random.uniform((), 1, 25, dtype=tf.int32),), 0, fid_max_val, dtype=np.int64)
-    #   features['label'] = tf.cast(tf.random.uniform((), 0, 2, dtype=tf.int32), tf.float32)
-    #   return features
-    
-    # return tf.data.Dataset.from_tensors(input_tensors()).repeat(102400000)\
-    #   .apply(tf.data.experimental.dense_to_ragged_batch(batch_size, True))\
-    #     .map(decomp_func).map(advanced_parse, tf.data.AUTOTUNE).prefetch(tf.data.AUTOTUNE)
 
   def model_fn(self, features, mode):
     with tf.device("/device:GPU:0"):
       for f_name in feature_names:
         self.create_embedding_feature_column(f_name)
-      print(features)
+
       tf_embeddings = [lookup_tf_embedding(features, f_name, emb_dim) for f_name in feature_names]
       embeddings = self.lookup_embedding_slice(features=feature_names,
                                               slice_name='vec',
@@ -146,16 +126,15 @@ class EmbeddingUpdateTask(MonolithModel):
           tf.keras.layers.Dense(32, activation="relu"),
           tf.keras.layers.Dense(1)
         ])(tf_embed_concat)
-      
+
       label = features['label']
       loss = tf.reduce_mean(tf.losses.mean_squared_error(pred, label))
       tf_loss = tf.reduce_mean(tf.losses.mean_squared_error(tf_pred, label))
       
       optimizer = tf.compat.v1.train.AdagradOptimizer(0.05)
       # with tf.device("/device:CPU:0"):
-      #   loss = tf.compat.v1.Print(loss, [loss], 'monolith loss')
-      #   tf_loss = tf.compat.v1.Print(tf_loss, [tf_loss], 'tf loss')
-          
+        # loss = tf.compat.v1.Print(loss, [loss], 'monolith loss')
+        # tf_loss = tf.compat.v1.Print(tf_loss, [tf_loss], 'tf loss')
       
       return EstimatorSpec(
         loss=loss + tf_loss,
